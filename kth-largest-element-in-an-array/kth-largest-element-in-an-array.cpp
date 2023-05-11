264548c1994ab59b0641ad0a7f4cda0e87ed0326
ashwik_aileni <ashwikreddyaileni999@gmail.com>
2023-05-11 08:01:54 +0530

    Time: 336 ms (5.08%) | Memory: 45.4 MB (91.84%) - LeetSync

---
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+
+        int st=0,en=nums.size()-1;
+        int n = nums.size();
+        int ind = 0;
+        while(true){
+            ind = findpartition(nums,st,en);
+            if(ind==n-k){break;}
+            else if(ind>n-k){
+                en = ind-1;
+            }else{
+                st = ind+1;
+            }
+        }
+        return nums[ind];
+        
+    }
+
+
+    int findpartition(vector<int>&nums,int left,int right){
+        int pivot =nums[left];
+        int l = left+1;
+        int r = right;
+
+        while(l<=r){
+            if(nums[l]>pivot && nums[r]<pivot){
+                swap(nums[l++],nums[r--]);
+            }
+            if(nums[l]<=pivot){
+                l++;
+            }
+            if(nums[r]>=pivot){
+                r--;
+            }
+        }
+        swap(nums[left],nums[r]);
+        return r;
+    }
+};
