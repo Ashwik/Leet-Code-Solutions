0ae7eecf292761838faed539f378f0fcee024e2a
ashwik_aileni <ashwikreddyaileni999@gmail.com>
2023-05-19 09:12:21 +0530

    Time: 61 ms (27.51%) | Memory: 17.2 MB (18.86%) - LeetSync

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -12,16 +12,17 @@ public:
         
         ListNode* temp1 = head1;
         ListNode* temp2 = head2;
+        unordered_set<ListNode*> st;
 
         while(temp1!=NULL){
-            temp2 = head2;
-            while(temp2!=NULL){
-                if(temp1==temp2){
-                    return temp1;
-                }
-                temp2 = temp2->next;
-            }
+            st.insert(temp1);
             temp1 = temp1->next;
+        }
+        while(temp2!=NULL){
+            if(st.find(temp2)!=st.end()){
+                return temp2;
+            }
+            temp2 = temp2->next;
         }
         return NULL;
     }
