4d5b525063e96fc6c7e8b3af525637db744cc701
ashwik_aileni <ashwikreddyaileni999@gmail.com>
2023-06-13 18:12:56 +0530

    Time: 3 ms (45.67%) | Memory: 8.3 MB (61.93%) - LeetSync

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -15,18 +15,20 @@ public:
         
         stack<TreeNode*> st;
         vector<int> ans;
-
+        
         while(!st.empty() || root!=NULL){
             if(root!=NULL){
                 st.push(root);
                 root= root->left;
             }else{
-                root = st.top();
+                root = st.top();st.pop();
                 ans.push_back(root->val);
-                st.pop();
                 root = root->right;
             }
+
         }
+
         return ans;
+
     }
 };
