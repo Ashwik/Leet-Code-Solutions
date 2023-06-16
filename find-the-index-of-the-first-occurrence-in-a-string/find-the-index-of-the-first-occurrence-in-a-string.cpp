1334789a1d4c0b5a5c0d02f5c3e14b8898b975c4
ashwik_aileni <ashwikreddyaileni999@gmail.com>
2023-06-16 14:04:10 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (94.78%) - LeetSync

--- find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -2,6 +2,11 @@ class Solution {
 public:
     int strStr(string str, string s) {
 
+       
+        if(s.length()==str.length()){
+            if(s==str){return 0;}   
+        }
+         if(s.length()>=str.length()){return -1;}
         bool flag;
         for(int i=0;i<str.length();i++){
             if(str[i]==s[0]){
