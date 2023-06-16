c3dad5235eba143cbb7b64a69e1f13fd70df5904
ashwik_aileni <ashwikreddyaileni999@gmail.com>
2023-06-16 14:31:19 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (22.81%) - LeetSync

--- find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,22 +1,51 @@
 class Solution {
 public:
+
+    void computelpsarray(vector<int>& lps,string& s){
+        int len=0,end=1;
+        lps[len]=0;
+
+        while(end<lps.size()){
+            if(s[end]==s[len]){
+                len++;
+                lps[end]=len;
+                end++;
+            }else{
+                if(len){
+                    len = lps[len-1];
+                }else{
+                    lps[end]=0;
+                    end++;
+                }
+            }
+        }
+    }
+
     int strStr(string str, string s) {
 
        
         if(s.length()==str.length()){
             if(s==str){return 0;}   
         }
-         if(s.length()>=str.length()){return -1;}
-        bool flag;
-        for(int i=0;i<str.length();i++){
-            if(str[i]==s[0]){
-                flag = true;
-                for(int j=1,k=i+1;j<s.length();j++,k++){
-                    if(s[j]!=str[k]){
-                        flag = false;break;
-                    }
+        if(s.length()>=str.length()){return -1;}
+        
+        int n = str.length();//main string
+        int m = s.length();//pattern
+        vector<int> lps(m);
+        computelpsarray(lps,s);
+        int i=0,j=0;
+        while(n-i>=m-j){
+            if(str[i]==s[j]){
+                i++;j++;
+            }
+            if(j==m){
+                return i-j;
+            }else if(i<n && str[i]!=s[j]){
+                if(j==0){
+                    i++;
+                }else{
+                    j = lps[j-1];
                 }
-                if(flag){return i;}
             }
         }
         return -1;
