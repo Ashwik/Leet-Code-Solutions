bc23fbe71e2950884fe5f62377d30fd97211da86
ashwik_aileni <ashwikreddyaileni999@gmail.com>
2024-08-27 16:18:45 +0530

    Time: 141 ms (77.12%) | Memory: 85.8 MB (90.85%) - LeetSync

--- 1985-maximum-subarray-min-product/maximum-subarray-min-product.cpp
+++ 1985-maximum-subarray-min-product/maximum-subarray-min-product.cpp
@@ -2,36 +2,6 @@ class Solution {
 public:
     int maxSumMinProduct(vector<int>& nums) {
         int n = nums.size();
-        vector<int> left(nums.size(),-1);
-        vector<int> right(nums.size(),n-1);
-
-        stack<int> st;
-
-        for(int i=0;i<n;i++){
-            while(!st.empty() && nums[i]<=nums[st.top()]){
-                st.pop();
-            }
-
-            if(!st.empty()){
-                left[i] = st.top();
-            }
-            st.push(i);
-        }
-
-        while(!st.empty()){
-            st.pop();
-        }
-
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && nums[i]<nums[st.top()]){
-                st.pop();
-            }
-
-            if(!st.empty()){
-                right[i] = st.top()-1;
-            }
-            st.push(i);
-        }
 
         vector<long long int> cnums(n);
         cnums[0] = nums[0];
@@ -43,14 +13,32 @@ public:
         long long int prod;
         long long int mod = pow(10,9)+7;
 
-        for(int i=0;i<nums.size();i++){
-            prod = cnums[right[i]];
-            if(left[i]!=-1){
-                prod -= cnums[left[i]];
+        stack<int> st;
+        int minval;
+
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[i]<=nums[st.top()]){
+                minval = nums[st.top()];
+                st.pop();
+                prod = cnums[i-1];
+                if(!st.empty()){
+                    prod -= cnums[st.top()];
+                }
+                ans = max(ans,prod*minval);
             }
-            prod = (prod*nums[i]);
-            ans = max(ans,prod);
+            st.push(i);
         }
+
+        while(!st.empty()){
+            minval = nums[st.top()];
+            st.pop();
+            prod = cnums[n-1];
+            if(!st.empty()){
+                prod -= cnums[st.top()];
+            }
+            ans = max(ans,prod*minval);
+        }
+        
         return ans%mod;
     }
 };
