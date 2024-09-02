aa328c8941cce16090fd46f2fdb5712fa35af154
ashwik_aileni <ashwikreddyaileni999@gmail.com>
2024-09-02 13:22:37 +0530

    Time: 206 ms (57.57%) | Memory: 155 MB (28.25%) - LeetSync

--- 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
@@ -12,28 +12,38 @@
 class Solution {
 public:
 
+    struct Node{
+        int minVal;
+        int maxVal; 
+        int sum;
+        Node(int minVal,int maxVal,int sum){
+            this->minVal = minVal;
+            this->maxVal = maxVal;
+            this->sum = sum;
+        }
+    };
     
-    vector<int> isValid(TreeNode* root, int &bstSum){
+    Node* isValid(TreeNode* root, int &bstSum){
 
-        if(root==NULL){return {INT_MAX,INT_MIN,0};}
+        if(root==NULL){return new Node(INT_MAX,INT_MIN,0);}
 
-        vector<int> leftTree = isValid(root->left, bstSum);
-        vector<int> rightTree = isValid(root->right, bstSum);
+        Node* leftTree = isValid(root->left, bstSum);
+        Node*  rightTree = isValid(root->right, bstSum);
         
         int val = root->val;
         
-        if(leftTree.size()==0 || rightTree.size() == 0 || val <= leftTree[1] || val >= rightTree[0]){
-            return {};
+        if(leftTree == NULL || rightTree == NULL || val <= leftTree->maxVal || val >= rightTree->minVal){
+            return NULL;
         }
         // for given root value it should between max value in leftsubtree and min value in right subtree
 
-        int sum = val + leftTree[2] + rightTree[2];
+        int sum = val + leftTree->sum + rightTree->sum;
         bstSum = max(sum,bstSum);
         
-        int mi = min(val, leftTree[0]);// leftTree[0] has the minimum value in left sub tree
-        int ma = max(val, rightTree[1]);// rightTree[1] has the maximum value in right sub tree
+        int mi = min(val, leftTree->minVal);// leftTree[0] has the minimum value in left sub tree
+        int ma = max(val, rightTree->maxVal);// rightTree[1] has the maximum value in right sub tree
 
-        return {mi,ma,sum};
+        return new Node(mi,ma,sum);
     }
 
     int maxSumBST(TreeNode* root) {
