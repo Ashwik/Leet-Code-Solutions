faefe1bfdc820242c6e32b52ae6021fef6971eeb
ashwik_aileni <ashwikreddyaileni999@gmail.com>
2024-09-24 01:12:49 +0530

    Time: 424 ms (47.31%) | Memory: 191.9 MB (46.60%) - LeetSync

--- 460-lfu-cache/lfu-cache.cpp
+++ 460-lfu-cache/lfu-cache.cpp
@@ -1,68 +1,61 @@
 class LFUCache {
 public:
 
-    int minfreq=0;
+    unordered_map<int,pair<int,int>> cache;
+    unordered_map<int,list<int>::iterator> listItrMap;
+    unordered_map<int,list<int>> freq;
+
     int capacity;
-    int size = 0;
-    unordered_map<int,pair<int,int>> cache;// key -> {value,count}
-    unordered_map<int,list<int>::iterator> listitmap; // key -> freqmap[ind].begin()+i
-    unordered_map<int,list<int>> freqmap;// count-> list<int> keys
+    int minFreq=0;
 
     LFUCache(int capacity) {
         this->capacity = capacity;
     }
     
-  
     int get(int key) {
-
         if(!cache.count(key)){
             return -1;
         }
-        updatefrequency(key);
+        updateFrequency(key);
         return cache[key].first;
     }
-
-    void remove(){
-        int leastfrequentkey = freqmap[minfreq].back(); // keyd in freqmap are orderd by most recently used 
-        cache.erase(leastfrequentkey);
-        listitmap.erase(leastfrequentkey);
-        freqmap[minfreq].pop_back();// least recently used key in same frequency keys
-    }
-
+    
     void put(int key, int value) {
-
         if(capacity<=0){
             return;
         }
 
         if(get(key)!=-1){
             cache[key].first = value;
-        }else{
-            if(cache.size()>=capacity){
-                remove();
-            }
-            cache[key] = {value,0};
-            freqmap[0].push_front(key);
-            listitmap[key] = freqmap[0].begin(); // point listiterator[key] to frequncy linked list
-            minfreq = 0;
-        }
-    }
-
-    void updatefrequency(int key){
-
-        int count = cache[key].second;
-        cache[key].second = count+1;
-        freqmap[count].erase(listitmap[key]); //listmap[key] contains freqmap[count].begin() + some unknown i
-        freqmap[count+1].push_front(key);
-        listitmap[key] = freqmap[count+1].begin();
-
-        if(freqmap[minfreq].size()==0){ // remove empty frequency list
-            freqmap.erase(minfreq);
-            minfreq++;
+            return;
         }
 
+        if(cache.size()==capacity){
+            int leastFrequentKey = freq[minFreq].back();
+            cache.erase(leastFrequentKey);
+            listItrMap.erase(leastFrequentKey);
+            freq[minFreq].pop_back();
+        }
+        cache[key] = {value,0};
+        freq[0].push_front(key);
+        listItrMap[key] = freq[0].begin();
+        minFreq = 0;
     }
 
+    void updateFrequency(int key){
+        int freqCount = cache[key].second;
+        cache[key].second = freqCount+1;
+
+        freq[freqCount].erase(listItrMap[key]);
+        
+        freq[freqCount+1].push_front(key);
+        listItrMap[key] = freq[freqCount+1].begin();
+
+        if(freq[minFreq].size()==0){
+            freq.erase(minFreq);
+            minFreq++;
+        }
+    }
 };
 
 /**
