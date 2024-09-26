fba97d44e0d8e7de9b492d6858ef0a3cebc89822
ashwik_aileni <ashwikreddyaileni999@gmail.com>
2024-09-26 11:12:11 +0530

    Time: 17 ms (85.55%) | Memory: 8.2 MB (90.09%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -21,8 +21,13 @@ public:
     }
 
     int longestCommonSubsequence(string text1, string text2) {
+        
+        
+        
         int m = text1.length();
         int n = text2.length();
+
+        if(m<n){return longestCommonSubsequence(text2, text1);}
 
         vector<vector<int>> dp(2, vector<int>(n+1,0));
 
@@ -32,8 +37,9 @@ public:
             for(int j=1;j<=text2.length();j++){     
                 if(text1[i-1]==text2[j-1]){
                     dp[curind][j] = 1 + dp[prevind][j-1];
+                }else{
+                    dp[curind][j] = max(dp[prevind][j], dp[curind][j-1]);
                 }
-                dp[curind][j] = max(dp[curind][j], max(dp[prevind][j], dp[curind][j-1]));
             }
         }
         return dp[m%2][n];
