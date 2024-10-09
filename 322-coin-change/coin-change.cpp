67e93a4086a901ba06f0667dabd32f400c51aee8
ashwik_aileni <ashwikreddyaileni999@gmail.com>
2024-10-09 22:35:37 +0530

    Time: 41 ms (44.32%) | Memory: 24.5 MB (45.18%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n+1,vector<int>(amount+1,INT_MAX));
-        for(int i=0;i<=n;i++){
+        vector<vector<int>> dp(2,vector<int>(amount+1,INT_MAX));
+        for(int i=0;i<2;i++){
             dp[i][0] = 0;
         }
         
-        for(int j=1;j<=amount;j++){
-            for(int i=1;i<=n;i++){
-                dp[i][j] = dp[i-1][j];
-                if(j>=coins[i-1] && dp[i][j-coins[i-1]]!=INT_MAX){
-                    dp[i][j] = min(dp[i][j], dp[i][j-coins[i-1]] + 1);
+        for(int i=1;i<=n;i++){
+            int cur = i%2;
+            int prev = (i-1)%2;
+            for(int j=1;j<=amount;j++){   
+                dp[cur][j] = dp[prev][j];
+                if(j>=coins[i-1] && dp[cur][j-coins[i-1]]!=INT_MAX){
+                    dp[cur][j] = min(dp[cur][j], dp[cur][j-coins[i-1]] + 1);
                 }
             }
         }
-        if(dp[n][amount]==INT_MAX){return -1;}
-        return dp[n][amount];
+        if(dp[n%2][amount]==INT_MAX){return -1;}
+        return dp[n%2][amount];
     }
 };
