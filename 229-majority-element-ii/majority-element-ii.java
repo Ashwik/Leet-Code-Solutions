9d8f877dce3dea28596b81733368e372d594c9ac
ashwik_aileni <ashwikreddyaileni999@gmail.com>
2024-12-28 14:12:48 +0530

    Time: 3 ms (71.85%) | Memory: 48.7 MB (8.22%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -3,34 +3,24 @@ class Solution {
         List<Integer> li = new ArrayList<>();
         int[] count = new int[2];
         int[] arr = new int[2];
+        arr[0]=nums[0];
+        arr[1]=nums[0];
+        count[0]=1;
         
-        for(int i=0;i<nums.length;i++){
-            if(count[0]==0 && count[1]==0){
-                arr[0]=nums[i];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==arr[0]){
                 count[0]++;
+            }else if(nums[i]==arr[1]){
+                count[1]++;
             }else if(count[0]==0){
-               if(arr[1]==nums[i]){
-                    count[1]++;
-               }else{
-                    count[0]++;
-                    arr[0]=nums[i];
-               }
+               arr[0]=nums[i];
+               count[0]++;
             }else if(count[1]==0){
-                if(arr[0]==nums[i]){
-                    count[0]++;
-               }else{
-                    count[1]++;
-                    arr[1]=nums[i];
-               }
+                arr[1]=nums[i];
+                count[1]++;
             }else{
-                if(nums[i]==arr[0]){
-                    count[0]++;
-                }else if(nums[i]==arr[1]){
-                    count[1]++;
-                }else{
-                    count[0]--;
-                    count[1]--;
-                }
+                count[0]--;
+                count[1]--;
             }
         }
         count[0]=0;
